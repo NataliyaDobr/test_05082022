e3de006fb5d7a1241bc98e795e4524c52333f377
NataliyaDobr <n.m.suvorova@mail.ru>
2022-08-07 20:59:52 +0300

    PrintArray

--- Program.cs
+++ Program.cs
@@ -3,11 +3,22 @@ string[] ArrayString2 = new string[ArrayString1.Length];
 
 void NewArray(string[] array1, string[] array2)
 {
-    for (int i = 0; i < ArrayString1; i++)
+    for (int i = 0; i < ArrayString1.Length; i++)
     {
     if(ArrayString1[i].Length <= 3)
         {
-        ArrayString2[count] = array1[i];
+        ArrayString2[i] = array1[i];
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+NewArray(ArrayString1, ArrayString2);
+PrintArray(ArrayString2);
